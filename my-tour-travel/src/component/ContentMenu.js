09163cc3631159ba7f0c5948c575b46c400d0e84
IBAD ULLAH KHAN <ibad50008@gmail.com>
2025-08-19 23:51:15 -0700

    nav bar done

--- my-tour-travel/src/component/ContentMenu.js
+++ my-tour-travel/src/component/ContentMenu.js
@@ -1,33 +1,33 @@
-export const ContentMenu= [
+export const ContentMenu = [
     {
-        title:"Home",
-        url:"/",
-        cname:"nav-links",
-        icon:" fa-solid fa-house-user"
+        title: "Home",
+        url: "/",
+        cname: "nav-links",
+        icon: " fa-solid fa-house-user"
     },
     {
-        title:"About",
-        url:"/About",
-        cname:"nav-links",
-        icon:" fa-solid fa-circle-info"
+        title: "About",
+        url: "/About",
+        cname: "nav-links",
+        icon: " fa-solid fa-circle-info"
     },
     {
-        title:"Services",
-        url:"/Service",
-        cname:"nav-links",
-        icon:" fa-solid fa-briefcase "
+        title: "Services",
+        url: "/Service",
+        cname: "nav-links",
+        icon: " fa-solid fa-briefcase "
     },
     {
-        title:"Contacts",
-        url:"/Contact",
-        cname:"nav-links",
-        icon:" fa-solid fa-address-book"
+        title: "Contacts",
+        url: "/Contact",
+        cname: "nav-links",
+        icon: " fa-solid fa-address-book"
     },
     {
-        title:"Signup",
-        url:"/singup",
-        cname:"nav-links-mobile",
+        title: "Signup",
+        url: "/singup",
+        cname: "nav-links-mobile",
 
     },
-    
+
 ]
--- my-tour-travel/src/component/Navbar.js
+++ my-tour-travel/src/component/Navbar.js
@@ -1,26 +1,37 @@
 import { Component } from "react";
 import "./NavbarStyleSheet.css";
-import {ContentMenu} from "./ContentMenu"
+import { ContentMenu } from "./ContentMenu"
 
 
 class Navbar extends Component {
 
-    render(){
+    state = { clicked: false }
+    handleclicked = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+    render() {
 
-        return(
+        return (
 
             <nav className="NavbarItems">
                 <h1 className="navbar-Logo">Trippy</h1>
-                <ul className="nav-menu">
-                   {ContentMenu.map((item,index)=>{
-                    return(
-                         <li key={index}>
-                        <a href={item.url}  className={item.cname}>
-                        <i className={item.icon}/> {item.title}
-                        </a>
-                    </li>
-                    )
-                   })}
+
+                <div className="menu-icons" onClick={this.handleclicked}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+
+                </div>
+
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                    {ContentMenu.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a href={item.url} className={item.cname}>
+                                    <i className={item.icon} /> {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                    <button>Signup</button>
                 </ul>
             </nav>
         )
